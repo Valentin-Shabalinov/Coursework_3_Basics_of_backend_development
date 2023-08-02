4e23d10e435e7adacc5437f97a8f38bb2900d54f
a1 <4267817@mail.ru>
2023-08-02 19:59:15 +0300

    Finish

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from utils import get_data, get_filtered_data, get_final_list, get_last_value
+from arrs import get_data, get_filtered_data, get_final_list, get_last_value
+
 
 def main():
     data = get_data()
     data = get_filtered_data(data)
     data = get_last_value(data)
     data = get_final_list(data)
+    
+    for trans in data:
+        print(trans)
 
 if __name__ == "__main__":
     main()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,17 +1,27 @@
-# import pytest
+import pytest
 
-# from utils import get_data
+from arrs import get_data, get_last_value, get_final_list
 
-# def test_get_data():
-#     data = get_data()
-#     assert isinstance(data, list)
 
-# # def test_get_filtered_data(test_data):
+def test_get_data():
+    data = get_data()
+    assert isinstance(data, list)
 
-# def test_get_last_value(test_data):
-#     data = get_last_value(test_data, 4)
-#     assert 
-    
+def test_get_last_value(test_data):
+    data = get_last_value(test_data)
+    assert [x['date'] for x in data] == [
+        '2019-12-08T22:46:21.935582', 
+        '2019-12-07T06:17:14.634890', 
+        '2019-11-19T09:22:25.899614', 
+        '2019-11-13T17:38:04.800051', 
+        '2019-11-05T12:04:13.781725'
+        ]
 
-# # def test_get_final_list():
-# #     assert utils.get_final_list
+def test_get_final_list(test_data):
+    assert get_final_list(test_data) == [
+        '08.12.2019 Открытие вклада\nОткрытие счета -> Счет **5907\n41096.24 USD\n',
+        '07.12.2019 Перевод организации\nVisa Classic  2842 87** **** 9012 -> Счет **3655\n48150.39 USD\n',
+        '19.11.2019 Перевод организации\nMaestro  7810 84** **** 5568 -> Счет **2869\n30153.72 руб.\n',
+        '13.11.2019 Перевод со счета на счет\nСчет **9794 -> Счет **8125\n62814.53 руб.\n',
+        '05.11.2019 Открытие вклада\nОткрытие счета -> Счет **8381\n21344.35 руб.\n'
+        ]
